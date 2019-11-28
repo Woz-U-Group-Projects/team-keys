2df6f5c840a196506d53fcb3c282577b1b034850
Reginald Keys <keysreggie@gmail.com>
2019-11-28 08:38:32 -0800

    added theability to create shouts

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,42 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello world!");
+});
+
+exports.getShouts = functions.https.onRequest((req, res) => {
+    admin
+    .firestore()
+    .collection('shouts')
+    .get()
+    .then((data) => {
+        let shouts = [];
+        data.forEach((doc) => {
+            shouts.push(doc.data());
+        });
+        return res.json(shouts);
+    })
+    .catch((err) => console.error(err));
+});
+
+exports.createShouts = functions.https.onRequest((req, res) => {
+    const newShouts = {
+        body: req.body.body,
+        userHandle: req.body.userHandle,
+        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    };
+
+    admin.firestore()
+    .collection('shouts')
+    .add(newShouts)
+    .then((doc) => {
+        res.json({ message: `document ${doc.id} created successfully`});
+    })
+    .catch((err) => {
+        res.status(500).json({ error: 'something not working right'});
+        console.error(err);
+    })
+});
